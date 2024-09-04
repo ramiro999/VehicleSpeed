e2515db0670e08e5c0e3b8438891168f73fa387f
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-09-04 11:53:03 -0500

    add changes - add solidAngle

--- lensAnalysis/FOV.py
+++ lensAnalysis/FOV.py
@@ -34,45 +34,53 @@ etendue = A * omega # etendue in m^2 sr
 
 # Field of view (FOV), Depth of field (DOF), and Ground Sampling Distance (GSD) 
 pixSize = np.sqrt(A) # pixel size; in meters
-AoV = 2 * np.arctan(pixSize / (2*f)) # angle of view in degrees (per pixel)
+AoV = 2 * np.degrees(np.arctan(pixSize / (2 * f))) # angle of view in degrees (per pixel)
 IFoV = pixSize / f # instantaneous field of view m/degrees
 
 circleConfusion = 2 * pixSize # Circle of confusion
 imageHeight = 1; # in meters
 
 distances = np.arange(1, 1001)
-DoF = np.zeros_like(range(1, 1001))
-Gr_Sam_Dis = np.zeros_like(range(1, 1001))
-IFoV_distance = np.zeros_like(range(1, 1001))
+DoF = np.zeros_like(distances, dtype=np.float64)
+Gr_Sam_Dis = np.zeros_like(distances, dtype=np.float64)
+IFoV_distance = np.zeros_like(distances, dtype=np.float64)  
 
 for i, d in enumerate(distances):
     # Depth of field
-    DoF[i] = 2 * f_number * circleConfusion * (d/f)**2
+    DoF[i] = 2 * f_number * circleConfusion * (d / f)**2
     # Ground Sampling Distance (GSD) in meters
     Gr_Sam_Dis[i] = (d * pixSize) / (f * imageHeight);
+    # Instantaneous Field of View (IFoV) in meters
     IFoV_distance[i] = 2 * (d * np.tan(np.deg2rad(AoV / 2)))
 
 # Plotting
-plt.figure()
-plt.plot(distances, DoF)
-plt.xlabel('Distance (m)')
-plt.ylabel('DOF (m)')
-plt.title('Depth of Field')
-plt.grid(True)
-plt.show()
+# Plotting DOF
+plt.figure(figsize=(12, 4))
 
-plt.figure()
-plt.plot(distances, IFoV_distance * 100)
+# Subplot 1: Depth of Field
+plt.subplot(1, 3, 1)
+plt.plot(distances, DoF, linewidth=2)
+plt.title('Depth of Field')
+plt.xlabel('Distance [m]')
+plt.ylabel('DOF [m]')
+plt.grid(True, which='both', linestyle='--', alpha=0.5)
+plt.yscale('log')
+
+# Subplot 2: Instantaneous Field of View (IFOV)
+plt.subplot(1, 3, 2)
+plt.plot(distances, IFoV_distance * 100, linewidth=2)
 plt.xlabel('Distance (m)')
 plt.ylabel('Object size (cm)')
-plt.title(f'AOV = {AoV:.2f} degrees')
-plt.grid(True)
-plt.show()
+plt.grid(True, which='both', linestyle='--', alpha=0.5)
+plt.title(f'AOV = {AoV:.2f}º')
 
-plt.figure()
-plt.plot(distances, Gr_Sam_Dis * 100)
+# Subplot 3: Ground Sampling Distance (GSD)
+plt.subplot(1, 3, 3)
+plt.plot(distances, Gr_Sam_Dis * 100, linewidth=2)
 plt.xlabel('Distance (m)')
 plt.ylabel('GSD (cm)')
-plt.title('Ground Sampling Distance')
 plt.grid(True)
+plt.title('Ground Sampling Distance GSD (cm)')
+
+plt.tight_layout()
 plt.show()
